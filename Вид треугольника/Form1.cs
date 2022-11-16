2e3a46887ab3010ba77b31e7a73d85f4bc3c224d
VictorGetmanenko <59647844+VictorGetmanenko@users.noreply.github.com>
2022-11-16 17:52:40 +0300

    Add files via upload

---
+++ Вид треугольника/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Вид_треугольника
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double ab, bc, ca;
+            ab = Convert.ToDouble(textBox1.Text);
+            bc = Convert.ToDouble(textBox2.Text);
+            ca = Convert.ToDouble(textBox3.Text);
+            if ((ab >= (bc + ca)) || (bc >= (ab + ca)) || (ca >= (ab + bc)))
+            {
+                label5.Text = "Треугольник с такими сторонами\n не существует";
+                return;
+            }
+            if (ab == bc && bc == ca && ca == ab)
+            {
+                label5.Text = "Треугольник равносторонний";
+                return;
+            }
+            if ((ab == bc)||(bc == ca)||(ca == ab))
+            {
+                label5.Text = "Треугольник равнобедренный";
+                return;
+            }
+            if ((ab*ab == (bc*bc + ca*ca)) || (bc*bc==(ca*ca + ab*ab)) || (ca*ca == (ab*ab + bc * bc)))
+            {
+                label5.Text = "Треугольник прямоугольный";
+                return;
+            }
+            label5.Text = "Треугольник разносторонний";
+        }
+    }
+}
---
+++ Квадратное уравнение/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Квадратное_уравнение
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a, b, c;
+            double D;
+            double x1, x2;
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox2.Text);
+            c = Convert.ToDouble(textBox3.Text);
+            D = b * b - 4 * a * c;
+            if (D < 0)
+            {
+                label5.Text = "Корней нет";
+                return;
+            }
+            if (D==0)
+            {
+                x1 = -b / (2 * a);
+                label5.Text = $"Один корень Х = {Math.Round(x1,3)}";
+                return;
+            }
+            if (D>0)
+            {
+                x1 = (-b + Math.Sqrt(D) / (2 * a));
+                x2 = (-b - Math.Sqrt(D) / (2 * a));
+                label5.Text = $"Два корня Х1 = {Math.Round(x1, 3)}  X2 = {Math.Round(x2, 3)}";
+                return;
+            }
+        }
+    }
+}
